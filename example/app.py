76242fb78c1cd5c0d62f60370c66b64b71dc624a
charley12009 <76769844+charley12009@users.noreply.github.com>
2023-03-16 09:29:35 +0800

    Update app.py

--- example/app.py
+++ example/app.py
@@ -68,11 +68,11 @@ def hanle_message(event):
         
 import os
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
+    #arg_parser = ArgumentParser(
+    #    usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    #)
+    #arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+    #arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    #options = arg_parser.parse_args()
 
     app.run(debug=options.debug, port=options.port)
