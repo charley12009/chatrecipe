c1aeaf97441854b491f0279b6b3f7571f4cc2a50
charley12009 <76769844+charley12009@users.noreply.github.com>
2023-03-17 16:16:23 +0800

    Update app.py

--- example/app.py
+++ example/app.py
@@ -86,12 +86,12 @@ def message_text(event):
         # 將新的回答作為之前的答案，供下一次迭代使用
         
 if __name__ == "__main__":
-    #arg_parser = ArgumentParser(
-    #    usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    #)
-    #arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
-    #arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    #options = arg_parser.parse_args()
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
 
-    app.run()
+    app.run(debug=options.debug, port=options.port)
     #debug=options.debug, port=options.port
