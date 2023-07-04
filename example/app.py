70183cfb3e33b85dfc1010c954cf2f8d947a3f94
charley12009 <76769844+charley12009@users.noreply.github.com>
2023-07-04 13:42:10 +0800

    Update app.py

--- example/app.py
+++ example/app.py
@@ -57,7 +57,7 @@ def message_text(event):
         if '食譜' in prompt:
             prompt += '輸出內容包括餐點名稱、材料、餐點做法，材料中要包含數量'
         if prompt == '@查詢當前食譜':
-            prompt='請給我當前菜單的所有食譜'
+            prompt='請給我剛剛菜單中所有餐點的所有食譜'
        # 將之前的答案和新的問題結合作為新的prompt
         prompt = f"{prev_answer} {prompt}"
         start_time=time.time()
